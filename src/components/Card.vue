4289233e4ac275be3559546d497865eab0f4984e
Francesco-Ostellari <francescoostellari@gmail.com>
2022-01-17 16:52:41 +0100

    add-card

---
+++ src/components/Card.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="col pb-3">
+    <div class="card_spotify">
+      <div class="container-img">
+        <img :src="image" :alt="name">
+      </div>
+      <div class="title">
+        {{title}}
+      </div>
+      <div class="author">
+        {{author}}
+      </div>
+      <div class="date">
+        {{year}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+	name: "Card",
+	props: [
+		'image',
+		'name',
+		'title',
+		'author',
+		'year'
+	]
+}
+</script>
+
+<style lang="scss">
+  .card_spotify {
+    width: 200px;
+    height: 350px;
+    padding: 20px;
+    background-color:#2E3A46;
+    .container-img{
+      img {
+        width: 100%;
+      }
+    }
+    .title {
+      color: white;
+      margin-top: 15px;
+      text-align: center;
+      font-size: 1.4em;
+      font-weight: bold;
+    }
+    .author {
+      text-align: center;
+      color: #788080;
+      font-weight: bold;
+    }
+    .date {
+      text-align: center;
+      color: #788080;
+      font-weight: bold;
+    }
+  }
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,223 +2,44 @@
   <main>
     <div class="container">
       <div class="row row-cols-5 p-5">
-        <div class="col pb-3">
-          <div class="card_spotify">
-            <div class="container-img">
-              <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
-            </div>
-            <div class="title">
-              NEW JERSEY
-            </div>
-            <div class="author">
-              Bon Jovi
-            </div>
-            <div class="date">
-              1988
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card_spotify">
-            <div class="container-img">
-              <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
-            </div>
-            <div class="title">
-              NEW JERSEY
-            </div>
-            <div class="author">
-              Bon Jovi
-            </div>
-            <div class="date">
-              1988
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card_spotify">
-            <div class="container-img">
-              <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
-            </div>
-            <div class="title">
-              NEW JERSEY
-            </div>
-            <div class="author">
-              Bon Jovi
-            </div>
-            <div class="date">
-              1988
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card_spotify">
-            <div class="container-img">
-              <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
-            </div>
-            <div class="title">
-              NEW JERSEY
-            </div>
-            <div class="author">
-              Bon Jovi
-            </div>
-            <div class="date">
-              1988
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card_spotify">
-            <div class="container-img">
-              <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
-            </div>
-            <div class="title">
-              NEW JERSEY
-            </div>
-            <div class="author">
-              Bon Jovi
-            </div>
-            <div class="date">
-              1988
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card_spotify">
-            <div class="container-img">
-              <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
-            </div>
-            <div class="title">
-              NEW JERSEY
-            </div>
-            <div class="author">
-              Bon Jovi
-            </div>
-            <div class="date">
-              1988
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card_spotify">
-            <div class="container-img">
-              <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
-            </div>
-            <div class="title">
-              NEW JERSEY
-            </div>
-            <div class="author">
-              Bon Jovi
-            </div>
-            <div class="date">
-              1988
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card_spotify">
-            <div class="container-img">
-              <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
-            </div>
-            <div class="title">
-              NEW JERSEY
-            </div>
-            <div class="author">
-              Bon Jovi
-            </div>
-            <div class="date">
-              1988
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card_spotify">
-            <div class="container-img">
-              <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
-            </div>
-            <div class="title">
-              NEW JERSEY
-            </div>
-            <div class="author">
-              Bon Jovi
-            </div>
-            <div class="date">
-              1988
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card_spotify">
-            <div class="container-img">
-              <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
-            </div>
-            <div class="title">
-              NEW JERSEY
-            </div>
-            <div class="author">
-              Bon Jovi
-            </div>
-            <div class="date">
-              1988
-            </div>
-          </div>
-        </div>
+        <Card 
+        v-for="(card, index) in cards" :key="index" :image="card.poster" :name="card.title" :title="card.title" :artist="card.author" :year="card.year"/>
       </div>
     </div>
   </main>
 </template>
 
 <script>
-// import axios from 'axios';
-// import CardNew from './CardNew.vue';
+import axios from 'axios';
+import Card from './Card.vue';
 
 export default {
   name: 'Main',
   components: {
-  
+    Card,
   },
   data() {
-    // return {
-    // };
-  },
-  created() {
-    console.log('created');
+    return {
+      cards: null
+    };
   },
   mounted() {
+    this.getCards();
   },
   methods: {
-    
+    getCards() {
+      axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+      .then((result) => {
+        this.cards = result.data.response;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
     },
-
+  }
 };
 </script>
 
 <style lang="scss">
-  .card_spotify {
-    width: 200px;
-    height: 350px;
-    padding: 20px;
-    background-color:#2E3A46;
-    .container-img{
-      img {
-        width: 100%;
-      }
-    }
-    .title {
-      color: white;
-      margin-top: 15px;
-      text-align: center;
-      font-size: 1.4em;
-      font-weight: bold;
-    }
-    .author {
-      text-align: center;
-      color: #788080;
-      font-weight: bold;
-    }
-    .date {
-      text-align: center;
-      color: #788080;
-      font-weight: bold;
-    }
-  }
+  
 </style>
